1abd7c87528214f8e039ea3fbf2e538dc70e3f30
adptra01 <m.a.syaputra.94@gmail.com>
2026-07-03 22:15:38 +0700

    fix: pindah htdocs/index.php ke file terpisah (hindari YAML syntax error)

--- deploy.sh
+++ deploy.sh
@@ -106,25 +106,7 @@ rsync -a public/ "$TEMP_DIR/htdocs/"
 
 # ---- Create optimized htdocs/index.php ----
 info "Membuat htdocs/index.php yang dioptimalkan..."
-cat > "$TEMP_DIR/htdocs/index.php" << 'INDEXPHP'
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../laravel/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../laravel/vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../laravel/bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
-INDEXPHP
+cp deploy-scripts/htdocs-index.php "$TEMP_DIR/htdocs/index.php"
 
 # ---- Copy .env.production jika ada, atau buat dari .env ----
 if [ -f ".env.production" ]; then
