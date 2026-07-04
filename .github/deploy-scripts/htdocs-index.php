558b5bbc92c586b012ddd95bc51cc3c39e1c3ba3
adptra01 <m.a.syaputra.94@gmail.com>
2026-07-04 15:56:41 +0700

    ganti _core/ ke laravel/ — manfaatkan folder dari Softaculous

--- .github/deploy-scripts/htdocs-index.php
+++ .github/deploy-scripts/htdocs-index.php
@@ -5,12 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/_core/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/laravel/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/_core/vendor/autoload.php';
+require __DIR__.'/laravel/vendor/autoload.php';
 
-$app = require_once __DIR__.'/_core/bootstrap/app.php';
+$app = require_once __DIR__.'/laravel/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
